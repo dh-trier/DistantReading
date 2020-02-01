692ad4557d99f2e2f8e7b6abfc85c480dd176037
yichen-tw <s7yichen@mail.de>
2020-02-01 21:17:39 +0100

    the configuration and folder zip file is updated

--- kolibri-addon-textgrid-import/kolibri-go.bat
+++
@@ -1,4 +0,0 @@
-java ^
-									-Dorg.apache.cxf.stax.maxChildElements=500000 -Xmx2048M ^
-									-Dfile.encoding=UTF-8 -jar kolibri-cli-9.0.12-SNAPSHOT.jar ^
-									-c %1
--- kolibri-addon-textgrid-import/kolibri-go.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-									java \
-									-Dorg.apache.cxf.stax.maxChildElements=500000 -Xmx2048M \
-									-Dfile.encoding=UTF-8 -jar kolibri-cli-9.0.12-SNAPSHOT.jar \
-									-c $1
