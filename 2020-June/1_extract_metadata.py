4d731bf2d4e41f7402c7f45fe428c21b1ce6d1bc
christofs <c.schoech@gmail.com>
2020-06-11 22:52:40 +0200

    split into Feb and June folders; first steps for simple Python-based solution

---
+++ 2020-June/1_extract_metadata.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+
+"""
+Script (a) for building a metadata table from ELTeC XML-TEI to prepare 
+data required for import into the TextGrid Repository.
+
+The XML-TEI files need to be valid against the ELTeC level1 schema.
+
+Usage: The only parameter you should need to adjust is the path
+encoded in the variable "inputdir" and "collection", to point it to the 
+appropriate language collection folder.
+
+Output: The script writes a file called "[collection].tsv" with the 
+metadata about the texts included in the collection to the folder "metadata".   
+
+Please send feedback to Christof at "schoech@uni-trier.de". 
+"""
+
+
+
+
+# === Import statements ===
+
+import os
+import re
+import glob
+from os.path import join
+from os.path import basename
+import pandas as pd
+from lxml import etree
+from collections import Counter
+
+
+# === Files and folders ===
+
+inputdir = join("input", "")
+collection = "ELTeC-fra"
+level = "level1"
+
+
+# === Parameters === 
+
+xpaths = {"xmlid" : "//tei:TEI/@xml:id", 
+          "title" : "//tei:titleStmt/tei:title/text()",
+          "gender" : "//tei:textDesc/eltec:authorGender/@key",
+          "size" : "//tei:textDesc/eltec:size/@key",
+          "reprints" : "//tei:textDesc/eltec:reprintCount/@key",
+          "timeslot" : "//tei:textDesc/eltec:timeSlot/@key",
+          "firstedition" : "//tei:bibl[@type='firstEdition']/tei:date/text()",
+          "digitalSource" : "//tei:bibl[@type='digitalSource']/tei:publisher/text()",
+          "language" : "//tei:langUsage/tei:language/@ident"}
+
+ordering = ["filename", "xmlid", "author", "title", "firstedition", "language", "gender", "size", "reprints", "timeslot"]
+
+sorting = ["firstedition", True] # column, ascending?
+
+
+# === Functions ===
+
+
+def open_file(teiFile): 
+    """
+    Open and parse the XML file. 
+    Returns an XML tree.
+    """
+    with open(teiFile, "r", encoding="utf8") as infile:
+        xml = etree.parse(infile)
+        return xml
+
+
+
+def get_metadatum(xml, xpath): 
+    """
+    For each metadata key and XPath defined above, retrieve the 
+    metadata item from the XML tree.
+    Note that the individual identifers for au-ids and title-ids 
+    are not split into individual columns.
+    """
+    try: 
+        namespaces = {'tei':'http://www.tei-c.org/ns/1.0',
+                      'eltec':'http://distantreading.net/eltec/ns'}       
+        metadatum = xml.xpath(xpath, namespaces=namespaces)[0]
+    except: 
+        metadatum = "NA"
+    # remove extra elements of the ELTeC title.
+    metadatum = re.sub(" : ELTeC edition", "", metadatum)
+    metadatum = re.sub(" : édition ELTeC", "", metadatum)
+    metadatum = re.sub(" : edition ELTeC", "", metadatum)
+    metadatum = re.sub(" : ELTeC edition", "", metadatum)
+    metadatum = re.sub(" : ELTeC Edition", "", metadatum)
+    # make sure language codes have three characters
+    metadatum = re.sub("fr$", "fra", metadatum)
+    metadatum = re.sub("en$", "eng", metadatum)
+    metadatum = re.sub("de$", "deu", metadatum)
+    return metadatum
+
+
+def get_authordata(xml): 
+    """
+    Retrieve the author field and split it into constituent parts.
+    Expected pattern: "name (alternatename) (birth-death)"
+    where birth and death are both four-digit years. 
+    The alternate name is ignored. 
+    Note that the first and last names are not split into separate
+    entries, as this is not always a trivial decision to make.
+    """
+    try: 
+        namespaces = {'tei':'http://www.tei-c.org/ns/1.0'}       
+        authordata = xml.xpath("//tei:titleStmt/tei:author/text()",
+                               namespaces=namespaces)[0]
+        author = re.search("(.*?) \(", authordata).group(1)
+        birth = re.search("\((\d\d\d\d)", authordata).group(1)
+        death = re.search("(\d\d\d\d)\)", authordata).group(1)
+    except: 
+        name = "NA"
+        birth = "NA"
+        death = "NA"        
+    return author,birth,death
+
+
+
+def save_metadata(metadata, metadatafile, ordering, sorting): 
+    """
+    Save all metadata to a CSV file. 
+    The ordering of the columns follows the list defined above.
+    """
+    metadata = pd.DataFrame(metadata)
+    metadata = metadata[ordering]
+    metadata = metadata.sort_values(by=sorting[0], ascending=sorting[1])
+    print(metadatafile)
+    with open(join(metadatafile), "w", encoding="utf8") as outfile: 
+        metadata.to_csv(outfile, sep="\t", index=None)
+
+
+# === Coordinating function ===
+
+def main(inputdir, collection, level, xpaths, ordering, sorting):
+    """
+    From a collection of ELTeC XML-TEI files,
+    create a CSV file with metadata about each file.
+    """
+    teiFolder = join(inputdir, collection, level, "*.xml")
+    metadatafile = join("metadata", collection+".tsv")
+    allmetadata = []
+    counter = 0
+    for teiFile in glob.glob(teiFolder): 
+        filename,ext = basename(teiFile).split(".")
+        print(filename)
+        try: 
+            if "schemas" not in filename:
+                counter +=1
+                keys = []
+                metadata = []
+                keys.append("filename")
+                metadata.append(filename)
+                xml = open_file(teiFile)
+                author,birth,death = get_authordata(xml)
+                keys.extend(["author", "birth", "death"])
+                metadata.extend([author, birth, death])
+                for key,xpath in xpaths.items(): 
+                    metadatum = get_metadatum(xml, xpath)
+                    keys.append(key)
+                    metadata.append(metadatum)
+                allmetadata.append(dict(zip(keys, metadata)))
+        except: 
+            print("ERROR!!!", filename)
+    print("FILES:", counter)
+    save_metadata(allmetadata, metadatafile, ordering, sorting)
+    
+main(inputdir, collection, level, xpaths, ordering, sorting)
---
+++ 2020-June/2_build_tgrfiles.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+"""
+Script for creating the various files required for import of data into 
+the TextGrid Repository. 
+
+The script requires the metadata file produced in the previous step and
+the full XML-TEI files to be uploaded. 
+
+Usage: The only parameter you should need to adjust is the path
+encoded in the variable "collection" to be worked on. 
+File preparation is one one language at a time. 
+
+Output: The script writes a collection of files to the output folder for
+the language collection concerned. 
+
+Please send feedback to Christof at "schoech@uni-trier.de". 
+"""
+
+
+
+
+# === Import statements ===
+
+import os
+import re
+import glob
+from os.path import join
+from os.path import basename
+import pandas as pd
+from collections import Counter
+import lxml.etree as ET
+
+
+
+# === Files and folders ===
+
+collection = "ELTeC-fra"
+level = "level1"
+
+
+
+# === Helper functions ===
+
+
+def read_metadatafile(metadatafile): 
+    with open(metadatafile, "r", encoding="utf8") as infile: 
+        metadata = pd.read_csv(infile, sep="\t", index_col="xmlid")
+        #print(metadata.head())
+    return metadata
+
+
+def read_template(templateid):
+    templatefile = join("templates", templateid)
+    with open(templatefile, "r", encoding="utf8") as infile: 
+        template = infile.read()
+        return template
+
+
+def save_template(template, templatefile): 
+    with open(join("output", templatefile), "w", encoding="utf8") as outfile: 
+        outfile.write(template)
+
+
+
+# === Functions to fill template files: one per language ===
+
+
+def fill_aggregation_meta(language):
+    templatefile = "-LLL.aggregation.meta"
+    template = read_template(join(templatefile))
+    template = re.sub("LLL", language, template)
+    templatefile = re.sub("LLL", language, templatefile)
+    save_template(template, templatefile)
+    
+
+# TODO: "LLL.aggregation"
+
+
+
+# === Functions to fill template files: one per text ===
+
+
+def fill_LLLNNN_edition_meta(xmlfile, counter, language, metadata):
+    # Read the empty templatefile
+    templatefile = "LLLNNN.edition.meta"
+    template = read_template(join("LLL", templatefile))
+    template = re.sub("LLL", language, template)
+    # Find information for the template
+    identifier, rest = basename(xmlfile).split("_")
+    author = metadata.loc[identifier, "author"]
+    title = metadata.loc[identifier, "title"]
+    # Fill information into the template
+    template = re.sub("LLL", language, template)   
+    template = re.sub("NNN", counter, template)   
+    template = re.sub("#author#", author, template)   
+    template = re.sub("#title#", title, template)   
+    # Adapt the templatefile's filename
+    templatefile = re.sub("LLL", language, templatefile)
+    templatefile = re.sub("NNN", counter, templatefile)
+    # Save the individual, filled-in templatefile
+    save_template(template, templatefile)
+
+
+
+# TODO: LLL/LLLNNN.edition
+
+# TODO: LLL/LLLNNN/-LLLNNN.xml
+
+# TODO: LLL/LLLNNN/-LLLNNN.xml.meta
+
+# TODO: LLL/LLLNNN/LLLNNN.work
+
+# TODO: LLL/LLLNNN/LLLNNN.work.meta
+
+
+
+# === Main ===
+
+
+def main(collection, level):
+    language = collection[-3:].upper()
+    metadatafile = join("metadata", collection+".tsv")
+    metadata = read_metadatafile(metadatafile)
+    xmlfiles = join("input", collection, level, "*.xml") 
+    fill_aggregation_meta(language)
+    counter  = 0
+    for xmlfile in glob.glob(xmlfiles):
+        counter +=1
+        counter = "{:03}".format(counter)
+        print(counter, basename(xmlfile))
+        fill_LLLNNN_edition_meta(xmlfile, counter, language, metadata)
+        counter = int(counter)
+
+main(collection, level)
+
+
